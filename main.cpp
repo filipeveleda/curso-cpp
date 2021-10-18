930a91035f0ee0a1fb7e1f76d30a35d6fdc835f9
Filipe V Mimbarcas <filipemimbarcas@gmail.com>
2021-10-18 09:05:53 -0300

    feat: Implementaçao da repetiçao de tentativas

--- main.cpp
+++ main.cpp
@@ -8,28 +8,38 @@ int main(){
   cout << "* Bem vindos ao jogo de advinhação *" << endl;
   cout << "************************************" << endl;
 
-  int numero_secreto = 42;
+  const int NUMERO_SECRETO = 42;
 
-  cout << "O numero secreto é "<< numero_secreto << ". Não conte para ninguém." << endl;
-
-  int chute;
-  cout << "Qual o seu chute? ";
-  cin >> chute;
+  // cout << "O numero secreto é "<< NUMERO_SECRETO << ". Não conte para ninguém." << endl;
+  bool nao_acertou = true;
+  int tentativas = 0;
 
 
-  bool acertou = chute == numero_secreto;
-  bool maior = chute > numero_secreto;
-  bool menor = chute < numero_secreto;
+  while(nao_acertou){
 
+    int chute;
+    tentativas++;
+    cout << "Tentativa: " << tentativas << endl;
+    cout << "Qual o seu chute? ";
+    cin >> chute;
 
-  if (acertou){
-    cout << "Parabéns! Você acertou o numero secreto!" << endl;
-  }else if(maior){
-    cout << "Chute foi maior que o numero secreto!" << endl;
-  }else if(menor){
-    cout << "Chute menor de o numero secreto!" << endl;
+    bool acertou = chute == NUMERO_SECRETO;
+    bool maior = chute > NUMERO_SECRETO;
+    bool menor = chute < NUMERO_SECRETO;
+
+    if (acertou){
+      cout << "Parabéns! Você acertou, o numero secreto é " << NUMERO_SECRETO << "." << endl;
+      nao_acertou = false;
+    }else if(maior){
+      cout << "Chute foi maior que o numero secreto!" << endl;
+    }else if(menor){
+      cout << "Chute menor de o numero secreto!" << endl;
+    }
+
   }
 
+  cout << "Fim de jogo!" << endl;
+  cout << "Você acertou o numero secreto em " << tentativas << " tentativas." << endl;
 
 }
 
